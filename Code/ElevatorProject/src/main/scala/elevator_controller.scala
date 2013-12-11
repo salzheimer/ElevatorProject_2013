c3bbf2342cd5095bb93b2237979008892dcc0625
Harrison Pickett <eeach@pobox.com>
2013-12-11 17:21:17 -0600

    updated elevator_controller, GuiCommands, and GuiGlobal

--- Code/ElevatorProject/src/main/scala/elevator_controller.scala
+++ Code/ElevatorProject/src/main/scala/elevator_controller.scala
@@ -56,33 +56,69 @@ class elevator_controller(top_floor:Int) extends Actor {
   var direction:Boolean = false
   val e_queue = new elevator_queue(top_floor)
 
+  // Give info from from motor.lineOut() better names:
+  // Returns the amount of feet of cable the motor has let out.
+  // 36 ft is floor 1
+  // 20 ft is floor 2
+  //  2 ft is floor 3
+  val floors_list:List[Int] = List(0, 36, 20, 2)
+
   // list requests the controller will respond to:
   case class floor_request(floor:Int)
   case class floor_clear(floor:Int, direction:Boolean)
   case class alarm(switch:Boolean)
   case class maintenance(switch:Boolean)
 
+  // list internal requests the controller will call on itself:
+  private case class floor_move(floor_number:Int, direction:Boolean)
+  private case class doors_toggle(door:guiDoor, state:String)
+
   // our main action loop
   def act = {
     while (true) {
       receive {
         case floor_request(floor) => start_floor_request(floor)
-        case floor_clear(floor, direction) => end_floor_request(floor,direction)
+        case floor_clear(floor, direction) => end_floor_request(floor, direction)
         case alarm(switch) => {
           if (switch) {
-            println("Fire Alarm: On")
+            println("fire alarm: on")
           } else {
-            println("Fire Alarm: Off")
+            println("fire alarm: off")
           }
         }
         case maintenance(switch) => {
           if (switch) {
-            println("Maintence Mode: On")
+            println("maintence mode: on")
           } else {
-            println("Maintenace Mode: Off")
+            println("maintenace mode: off")
           }
         }
-        case _ => {println("Unknown Request")}
+        case floor_move(floor_number, direction) => {
+          while(Motor.lineOut() != floors_list(floor_number)) {
+            if (direction) {
+              Motor.up()
+            } else {
+              Motor.down()
+            }
+          }
+          Motor.stop()
+          println("sending drop passenger")
+          drop_passenger(floor_number)
+        }
+        case doors_toggle(door, state) => {
+          println("recieved " + door + state)
+          state match {
+            case "open" => {
+              door.open()
+              println("from doors_toggle: doors open")
+            }
+            case "close" => {
+              door.close()
+              println("from doors_toggle: doors closed")
+            }
+          }
+        }
+        case _ => {println("unknown request")}
       }
     }
   }
@@ -104,10 +140,12 @@ class elevator_controller(top_floor:Int) extends Actor {
     required_direction match {
       case None => {
         // call "drop-off"
-        println("Same floor: call drop-off")
+        println("same floor: call drop-off")
       }
       case _ => {
+        println("start floor request")
         e_queue.floor_request_set(floor, required_direction.get)
+        guiGlobals.elev_con ! floor_move(floor, required_direction.get)
       }
     }
   }
@@ -115,5 +153,25 @@ class elevator_controller(top_floor:Int) extends Actor {
   def end_floor_request(floor_number:Int, direction:Boolean) = {
     // send e_queue floor number and current direction
     e_queue.floor_request_clear(floor_number, direction)
+  }
+
+  def drop_passenger(floor_number:Int) = {
+    // this is for testing, we'll need to repurpose it elsewhere
+    println("starting drop passenger")
+
+
+    var door = guiGlobals.door1
+
+    floor_number match {
+      case 1 => door = guiGlobals.door1
+      case 2 => door = guiGlobals.door2
+      case 3 => door = guiGlobals.door3
+    }
+
+    println("open doors")
+    guiGlobals.elev_con ! doors_toggle(door, "open")
+    Thread.sleep(10000)
+    println("close doors")
+    guiGlobals.elev_con ! doors_toggle(door, "close")
   }
 }
